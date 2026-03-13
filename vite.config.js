367ee733d412d844cbde2f0b0d723093596abb2b
Mehmet Sunguralp <mehmetsunguralp54@gmail.com>
2026-03-13 13:37:48 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	assetsInclude: ["**/*.hdr", "**/*.glb", "**/*.gltf"], // Add this line
+	base: "/3d-car-animation/",
 });
